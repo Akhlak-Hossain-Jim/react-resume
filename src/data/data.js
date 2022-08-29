26d5810a972e7fabc33c4f7c239eccdb699d9df7
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-30 00:56:31 +0600

    💅🛠️🏷️📕added

---
+++ src/data/data.js
@@ -0,0 +1,168 @@
+// iconType can only be:
+//      "web"
+//      "call"
+//      "mail"
+//      "github"
+//      "linkedin"
+//default is web
+
+const DATA = {
+  background: "#e0e0e0",
+  pageColor: "",
+  textColor: "",
+  linkColor: "",
+  name: "Joshua Allum",
+  about: "", // it wont be visible on the site
+  headerLinks: [
+    {
+      text: "jallum.xyz",
+      url: "https://sakl.com",
+      iconType: "web",
+    },
+    {
+      text: "jallum.xyz",
+      url: "https://sakl.com",
+      iconType: "web",
+    },
+    {
+      text: "jallum.xyz",
+      url: "https://sakl.com",
+      iconType: "web",
+    },
+    {
+      text: "jallum.xyz",
+      url: "https://sakl.com",
+      iconType: "web",
+    },
+    {
+      text: "jallum.xyz",
+      url: "https://sakl.com",
+      iconType: "web",
+    },
+  ],
+  WorkExperience: [
+    {
+      logo: "https://resume.jallum.xyz/static/media/sellyourstuff-logo.fc56196c.jpg",
+      name: "Sell Your Stuff Ltd",
+      location: "POS, Trinidad",
+      timePeriod: "June 2019-Present",
+      designation: "CO-FOUNDER & CTO",
+      description: [
+        "Building e-commerce platform for local small businesses that leverages WiPay and UPSL APIs to handle online payments and delivery respectively.",
+        "Launched beta version with 20+ stores and facilitated several transactions and deliveries in a fully automated fashion.",
+      ],
+    },
+    {
+      logo: "https://resume.jallum.xyz/static/media/sellyourstuff-logo.fc56196c.jpg",
+      name: "Sell Your Stuff Ltd",
+      location: "POS, Trinidad",
+      timePeriod: "June 2019-Present",
+      designation: "CO-FOUNDER & CTO",
+      description: [
+        "Building e-commerce platform for local small businesses that leverages WiPay and UPSL APIs to handle online payments and delivery respectively.",
+        "Launched beta version with 20+ stores and facilitated several transactions and deliveries in a fully automated fashion.",
+      ],
+    },
+    {
+      logo: "https://resume.jallum.xyz/static/media/sellyourstuff-logo.fc56196c.jpg",
+      name: "Sell Your Stuff Ltd",
+      location: "POS, Trinidad",
+      timePeriod: "June 2019-Present",
+      designation: "CO-FOUNDER & CTO",
+      description: [
+        "Building e-commerce platform for local small businesses that leverages WiPay and UPSL APIs to handle online payments and delivery respectively.",
+        "Launched beta version with 20+ stores and facilitated several transactions and deliveries in a fully automated fashion.",
+      ],
+    },
+  ],
+  Educations: [
+    {
+      degreeEarned: "Higher Secondary School Certificate",
+      institute: "Dhaka Commerce College",
+      graduationDate: "December 2021",
+      details: "",
+      results: [
+        {
+          title: "Grade Point Average",
+          grade: "77.28%",
+        },
+      ],
+    },
+    {
+      degreeEarned: "Secondary School Certificate",
+      institute: "Govt. Mohammedpur Model School and College",
+      graduationDate: "April 2019",
+      details: "",
+      results: [
+        {
+          title: "Grade Point Average",
+          grade: "69.28%",
+        },
+      ],
+    },
+  ],
+  Achievements: [
+    {
+      title: "Frontend mentor weekly 4th",
+      url: "https://www.frontendmentor.io/profile/Akhlak-Hossain-Jim",
+      date: "2022",
+    },
+    {
+      title: "CSSBattle 381",
+      url: "https://cssbattle.dev/player/ahjim",
+      date: "2022",
+    },
+    {
+      title: "3rd GSSDC National 2020",
+      url: "",
+      date: "2020",
+    },
+    {
+      title: "JUDO National 2020 Champion",
+      url: "",
+      date: "2020",
+    },
+    {
+      title: "DCDS 2019 Semi Finalist",
+      url: "",
+      date: "2019",
+    },
+  ],
+  Skills: [
+    {
+      title: "3+ years experience & shipped products",
+      skillSet:
+        "• React.js • HTML • CSS • Sass/scss • JSON • Git • Styled-components • i18 • Bootstrap • Tailwind • MUI • JavaScript • Next.js • Axios • Adobe XD • Figma • Heroku • Firebase • JAMstack • Linux",
+    },
+    {
+      title: "Some experience & comfortable with",
+      skillSet: "• Vue.js • PS • AI • Python • Java • C# • Numpy • Pandas",
+    },
+  ],
+  ClubActivity: [
+    {
+      name: "DCCDS",
+      url: "https://www.facebook.com/InfoDCCDS",
+      period: "2019-21",
+      work: "Debating in Both Asian and British Parliamentary in Bangla & English. • Helping junior debaters to grow in their debate careers.",
+    },
+    {
+      name: "DAGMC",
+      url: "https://www.facebook.com/DebatingAssociationofGovtModelCollege",
+      period: "2017-19",
+      work: "Debating in Both Asian and British Parliamentary in Bangla & English. • Helping junior debaters to grow in their debate careers.",
+    },
+  ],
+  Projects: [
+    {
+      name: "Web Book app",
+      url: "https://book-the-waves-virginia-woolf.web.app/",
+      using: "Reactjs, Styled-Components & firebase",
+      date: "2021",
+      details: [
+        "The waves by Virginia Woolf is a public domain book, Web app Created by me with some great features. Like:Changing theme, Remembering Last reading, Text size adjustment, Reading offline",
+      ],
+    },
+  ],
+};
+export default DATA;

6c58535b93c2d06631866e3ff132418cdaad75ed
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-30 00:56:51 +0600

    💅🛠️📕done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import Head from "./Components/Head";
+import Header from "./Components/Header";
+import MainData from "./Components/MainData";
+import Projects from "./Components/Projects";
+import DATA from "./data/data";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    document.body.style.background = DATA.background
+      ? DATA.background
+      : "#e0e0e0";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Head data={DATA} />
+      <Container
+        pageColor={DATA.pageColor && DATA.pageColor}
+        color={DATA.textColor && DATA.textColor}
+        linkColor={DATA.linkColor && DATA.linkColor}
+      >
+        <Header data={DATA} />
+        <div className="body">
+          <MainData data={DATA} />
+          <Projects projects={DATA.Projects} />
+        </div>
+      </Container>
+    </>
   );
 }
 
-export default App;
+const Container = styled.main`
+  width: 216mm;
+  height: 279mm;
+  margin: 5mm auto;
+  padding: 32px;
+  overflow: hidden;
+  position: relative;
+  page-break-after: always;
+  background-color: ${({ pageColor }) => (pageColor ? pageColor : "white")};
+  box-shadow: 0 0.5mm 2mm rgb(0 0 0 / 30%);
+  color: ${({ color }) => (color ? color : "#000")};
+  a {
+    color: ${({ linkColor }) => (linkColor ? linkColor : "#000")};
+  }
+`;
